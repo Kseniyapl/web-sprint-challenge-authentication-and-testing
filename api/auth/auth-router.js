ae067f047b447c1606801d1c14cd6ec9f378b67d
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-16 23:19:06 -0500

    log in woking with middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -20,31 +20,22 @@ router.post('/register', validateCreds, checkUsernameFree, (req, res, next) => {
     .catch(next) 
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+router.post('/login', checkUsernameExists, validateCreds, (req, res, next) => {
+  
+  let { username, password } = req.body
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
+  Users.findBy({ username })
+  .then(([user]) => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = tokenBuilder(user)
+        res.status(200).json({
+          message: `Welcome back ${user.username}...`,
+          token,
+        })
+      } else {
+        next({ status: 401, message: 'Invalid Credentials' })
       }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
-});
-
+    })
+    .catch(next)
+})  
 module.exports = router;
